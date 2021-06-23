9ad6c8838f09bfed1b6b01cae4c83e962f50d59e
DerekLarson77 <DerekLarson77@gmail.com>
2021-06-23 13:13:08 -0700

    Data displaying

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,3 +2,18 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+data.forEach(function(ufoData) {
+
+    var row = tbody.append("tr");
+
+    Object.entries(ufoData).forEach(function([key, value]) {
+        
+        console.log(key, value);
+        var cell = row.append("td");
+
+        cell.text(value);
+    });
+
+});
